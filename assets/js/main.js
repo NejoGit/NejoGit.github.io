407e54130545426698dc08c19576ec54b8583686
Harold Lancheros <hlancherosalvarez@gmail.com>
2023-03-09 20:21:07 -0600

    landing page first edit

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,22 +1,22 @@
 /*=============== SHOW MENU ===============*/
-const showMenu = (toggleId, navId) =>{
+const showMenu = (toggleId, navId) => {
     const toggle = document.getElementById(toggleId),
-    nav = document.getElementById(navId)
-    
+        nav = document.getElementById(navId)
+
     // Validate that variables exist
-    if(toggle && nav){
-        toggle.addEventListener('click', ()=>{
+    if (toggle && nav) {
+        toggle.addEventListener('click', () => {
             // We add the show-menu class to the div tag with the nav__menu class
             nav.classList.toggle('show-menu')
         })
     }
 }
-showMenu('nav-toggle','nav-menu')
+showMenu('nav-toggle', 'nav-menu')
 
 /*=============== REMOVE MENU MOBILE ===============*/
 const navLink = document.querySelectorAll('.nav__link')
 
-function linkAction(){
+function linkAction() {
     const navMenu = document.getElementById('nav-menu')
     // When we click on each nav__link, we remove the show-menu class
     navMenu.classList.remove('show-menu')
@@ -26,17 +26,17 @@ navLink.forEach(n => n.addEventListener('click', linkAction))
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
 const sections = document.querySelectorAll('section[id]')
 
-function scrollActive(){
+function scrollActive() {
     const scrollY = window.pageYOffset
 
-    sections.forEach(current =>{
+    sections.forEach(current => {
         const sectionHeight = current.offsetHeight,
-              sectionTop = current.offsetTop - 50,
-              sectionId = current.getAttribute('id')
+            sectionTop = current.offsetTop - 50,
+            sectionId = current.getAttribute('id')
 
-        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+        if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
             document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link')
-        }else{
+        } else {
             document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active-link')
         }
     })
@@ -44,18 +44,18 @@ function scrollActive(){
 window.addEventListener('scroll', scrollActive)
 
 /*=============== CHANGE BACKGROUND HEADER ===============*/
-function scrollHeader(){
+function scrollHeader() {
     const nav = document.getElementById('header')
     // When the scroll is greater than 80 viewport height, add the scroll-header class to the header tag
-    if(this.scrollY >= 80) nav.classList.add('scroll-header'); else nav.classList.remove('scroll-header')
+    if (this.scrollY >= 80) nav.classList.add('scroll-header'); else nav.classList.remove('scroll-header')
 }
 window.addEventListener('scroll', scrollHeader)
 
 /*=============== SHOW SCROLL UP ===============*/
-function scrollUp(){
+function scrollUp() {
     const scrollUp = document.getElementById('scroll-up');
     // When the scroll is higher than 560 viewport height, add the show-scroll class to the a tag with the scroll-top class
-    if(this.scrollY >= 560) scrollUp.classList.add('show-scroll'); else scrollUp.classList.remove('show-scroll')
+    if (this.scrollY >= 560) scrollUp.classList.add('show-scroll'); else scrollUp.classList.remove('show-scroll')
 }
 window.addEventListener('scroll', scrollUp)
 
@@ -63,6 +63,8 @@ window.addEventListener('scroll', scrollUp)
 const themeButton = document.getElementById('theme-button')
 const darkTheme = 'dark-theme'
 const iconTheme = 'bx-toggle-right'
+const svgGradient = document.getElementById('b')
+
 
 // Previously selected topic (if user selected)
 const selectedTheme = localStorage.getItem('selected-theme')
@@ -74,9 +76,18 @@ const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'bx-tog
 
 // We validate if the user previously chose a topic
 if (selectedTheme) {
-  // If the validation is fulfilled, we ask what the issue was to know if we activated or deactivated the dark
-  document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme)
-  themeButton.classList[selectedIcon === 'bx-toggle-left' ? 'add' : 'remove'](iconTheme)
+    // If the validation is fulfilled, we ask what the issue was to know if we activated or deactivated the dark
+    document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme)
+    themeButton.classList[selectedIcon === 'bx-toggle-left' ? 'add' : 'remove'](iconTheme)
+
+    // Update the SVG gradient color based on current theme
+    if (getCurrentTheme() === 'dark') {
+      svgGradient.children[1].setAttribute('stop-color', 'rgba(255, 190, 170, 1)');
+      svgGradient.children[0].setAttribute('stop-color', 'rgba(255, 190, 170, 1)');
+    } else {
+      svgGradient.children[1].setAttribute('stop-color', 'rgba(255, 190, 170, 0.1)');
+      svgGradient.children[0].setAttribute('stop-color', 'rgba(255, 255, 255, 0)');
+    }
 }
 
 // Activate / deactivate the theme manually with the button
@@ -84,7 +95,18 @@ themeButton.addEventListener('click', () => {
     // Add or remove the dark / icon theme
     document.body.classList.toggle(darkTheme)
     themeButton.classList.toggle(iconTheme)
+
+    // Update the SVG gradient color based on current theme
+    if (getCurrentTheme() === 'dark') {
+        svgGradient.children[2].setAttribute('stop-color', 'rgba(44, 42, 38, 1)');
+        svgGradient.children[1].setAttribute('stop-color', 'rgba(44, 42, 38, 1)');
+        svgGradient.children[0].setAttribute('stop-color', 'rgba(0, 0, 0, 0)');
+      } else {
+        svgGradient.children[2].setAttribute('stop-color', 'rgba(255, 110, 64, 1)');
+        svgGradient.children[1].setAttribute('stop-color', 'rgba(255, 190, 170, 0.1)');
+        svgGradient.children[0].setAttribute('stop-color', 'rgba(255, 255, 255, 0)');
+      }
     // We save the theme and the current icon that the user chose
-    localStorage.setItem('selected-theme', getCurrentTheme())
-    localStorage.setItem('selected-icon', getCurrentIcon())
+    localStorage.setItem('selected-theme', getCurrentTheme());
+    localStorage.setItem('selected-icon', getCurrentIcon());
 })
